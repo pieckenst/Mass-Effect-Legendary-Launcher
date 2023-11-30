9b91ddfec8d879ab2f564b11b8dd223bd1aa1b70
Andrey Savich <46422808+pieckenst@users.noreply.github.com>
2023-11-30 14:54:30 +0300

    Update Program.cs

--- MELE-launcher/Program.cs
+++ MELE-launcher/Program.cs
@@ -22,7 +22,7 @@ namespace MELE_launcher
                 @"  | |__/ _` | || | ' \/ _| ' \/ -_) '_|                                                                               ",
                 @"  |____\__,_|\_,_|_||_\__|_||_\___|_|                                                                                 ",
             };
-            Console.WindowWidth = 200;
+            Console.WindowWidth = 250;
             Console.WriteLine("\n\n");
             
             
